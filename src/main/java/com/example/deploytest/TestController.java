d50cc63fe5597a0d699b878ae41da668829d63fd
dsa04156 <dsa04156@naver.com>
2024-09-06 16:50:45 +0900

    d

---
+++ src/main/java/com/example/deploytest/TestController.java
@@ -0,0 +1,13 @@
+package com.example.deploytest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+  @GetMapping("/")
+  public String HelloWorld(){
+    return "Hello World";
+
+  }
+}
